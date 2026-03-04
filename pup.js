741406f77de73fa96d9d3c046f11a56e5cf702b8
Sebastian Schilling <schilling@cls.rwth-aachen.de>
2026-03-04 18:40:59 +0100

    fix: added executable path to puppeteer launch for cronjob

--- pup.js
+++ pup.js
@@ -31,6 +31,7 @@ module.exports = {
     
     const browser = await puppeteer.launch({
       headless: true,
+      executablePath: '/usr/bin/chromium',
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
